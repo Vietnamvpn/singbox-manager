fddc3440adfbdec6c6576d60fa659193d2bd1435
vietnamvpn <kangst24h@gmail.com>
2026-07-28 16:23:56 +0800

    Rename domain strategy options for consistency in add.sh

--- users/add.sh
+++ users/add.sh
@@ -68,8 +68,8 @@ read -p "Lựa chọn (1-5) [Mặc định: 1]: " ip_choice
 case $ip_choice in
     2) STRATEGY="prefer_ipv4" ;;
     3) STRATEGY="prefer_ipv6" ;;
-    4) STRATEGY="only_ipv4" ;;
-    5) STRATEGY="only_ipv6" ;;
+    4) STRATEGY="ipv4_only" ;;
+    5) STRATEGY="ipv6_only" ;;
     *) STRATEGY="" ;;
 esac
 
